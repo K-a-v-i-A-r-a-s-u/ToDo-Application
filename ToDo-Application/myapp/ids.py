469b3e2c84c2ab8343acf1b07396f909466c9cd1
K-a-v-i-A-r-a-s-u <kaviarasu.2026@gmail.com>
2024-04-25 20:10:53 +0530

    ToDo-Application

---
+++ ToDo-Application/myapp/ids.py
@@ -0,0 +1,46 @@
+# a=10
+# d=10.1
+# b=256
+# c=a+b
+
+
+
+
+# print(id(a))
+# print(id(d))
+# # print(id(b))
+# print(id(c))
+
+l=[4,1,3]
+l1=[1,2,3]
+
+t1=(1,2,3)
+t2=(1,2,3)
+
+d1={"fgh":"fdfg","name":2}
+d2={"fgh":"fdfg","name":2}
+
+# print(id(l))
+# print(id(l1))
+# print("-----------------")
+# print(id(t1))
+# print(id(t2))
+# print("-----------------")
+# print(id(d1))
+print(id(d2))
+print(id(d1))
+
+# print(id(l))
+# print(id(l1))
+
+# print(id(l[1]))
+# print(id(l1[0]))
+
+# print(id(t1[0]))
+# print(id(t2[0]))
+
+print(id(d1["fgh"]))
+print(id(d2["fgh"]))
+
+
+
